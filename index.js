c6a2ca8da46351c3496da7f8e5473d8b900bfa2a
Lexandrina90 <Lexandrina90@gmail.com>
2023-05-11 17:25:23 +0300

    code refactored (add connectedCallback etc)

--- index.js
+++ index.js
@@ -2,47 +2,93 @@ class Stepper extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: 'open' });
+    this._value = 0;
+    this.step = 1;
+    this.fill = '#2b2220';
+    this.btnBackground = '#2b2220';
+    this.minusPlusBackground = '#ffb4a4';
+  }
 
-    const initialValue = parseFloat(this.getAttribute('initial-value')) || 0;
-    const step = parseFloat(this.getAttribute('step')) || 1;
-    const fill = this.getAttribute('fill') === 'true' ? '#ffb4a4' : '#2b2220';
-    const btnBackground = fill === '#ffb4a4' ? '#ffb4a4' : '#2b2220';
-    const minusPlusBackground = fill === '#ffb4a4' ? '#2b2220' : '#ffb4a4';
-
+  connectedCallback() {
+    this.initialValue = parseFloat(this.getAttribute('initial-value')) || 0;
+    this.step = parseFloat(this.getAttribute('step')) || 1;
+    this.fill = this.getAttribute('fill') === 'true' ? '#ffb4a4' : '#2b2220';
+    this.btnBackground = this.fill === '#ffb4a4' ? '#ffb4a4' : '#2b2220';
+    this.minusPlusBackground = this.fill === '#ffb4a4' ? '#2b2220' : '#ffb4a4';
 
     const container = document.createElement('div');
     container.setAttribute('class', 'stepper-container');
 
     container.innerHTML = `
-      <button class="decrement-btn" style="background: ${btnBackground}">
-        <div class="decrement-btn-minus" style="background: ${btnBackground}">
-          <div class="decrement-btn-minus-h" style="background: ${minusPlusBackground}"></div>
-        </div>
-      </button>
-      <div class="value-display">${initialValue}</div>
-      <button class="increment-btn" style="background: ${btnBackground}">
-        <div class="increment-btn-plus" style="background: ${btnBackground}">
-          <div class="increment-btn-plus-v" style="background: ${minusPlusBackground}"></div>
-          <div class="increment-btn-plus-h" style="background: ${minusPlusBackground}"></div>
-        </div>
-      </button>
-    `;
+        <button class="decrement-btn" style="background: ${this.btnBackground}">
+          <div class="decrement-btn-minus" style="background: ${this.btnBackground}">
+            <div class="decrement-btn-minus-h" style="background: ${this.minusPlusBackground}"></div>
+          </div>
+        </button>
+        <div class="value-display">${this.initialValue}</div>
+        <button class="increment-btn" style="background: ${this.btnBackground}">
+          <div class="increment-btn-plus" style="background: ${this.btnBackground}">
+            <div class="increment-btn-plus-v" style="background: ${this.minusPlusBackground}"></div>
+            <div class="increment-btn-plus-h" style="background: ${this.minusPlusBackground}"></div>
+          </div>
+        </button>
+      `;
 
-    const decrementBtn = container.querySelector('.decrement-btn');
+    this.shadowRoot.innerHTML = '';
+    this.shadowRoot.appendChild(container);
+
+    this.updateStyles();
+    this.updateDisplay();
+    this.addEventListeners();
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+
+    switch (name) {
+      case 'initial-value':
+        this.initialValue = parseFloat(newValue) || 0;
+        this.updateDisplay();
+        break;
+      case 'step':
+        this.step = parseFloat(newValue) || 1;
+        break;
+      case 'fill':
+        this.fill = newValue === 'true' ? '#ffb4a4' : '#2b2220';
+        this.btnBackground = this.fill === '#ffb4a4' ? '#ffb4a4' : '#2b2220';
+        this.minusPlusBackground = this.fill === '#ffb4a4' ? '#2b2220' : '#ffb4a4';
+        this.updateStyles();
+        break;
+      default:
+        break;
+    }
+  }
+  static get observedAttributes() {
+    return ['initial-value', 'step', 'fill'];
+  }
+
+
+
+  addEventListeners() {
+    const decrementBtn = this.shadowRoot.querySelector('.decrement-btn');
+    const incrementBtn = this.shadowRoot.querySelector('.increment-btn');
+
     const decrementButtonClickHandler = () => {
-      this.decrement(step);
+      this.decrement(this.step);
     };
     decrementBtn.addEventListener('click', decrementButtonClickHandler);
+    this.decrementButtonClickHandler = decrementButtonClickHandler;
 
-    const incrementBtn = container.querySelector('.increment-btn');
     const incrementButtonClickHandler = () => {
-      this.increment(step);
-    };
-    
+      this.increment(this.step);
+    }
     incrementBtn.addEventListener('click', incrementButtonClickHandler);
+    this.incrementButtonClickHandler = incrementButtonClickHandler;
+  }
 
 
-
+  updateStyles() {
     //CSS-style
     const style = document.createElement('style');
     console.log(style.isConnected);
@@ -68,19 +114,19 @@ class Stepper extends HTMLElement {
         font-size: 14px;
         border-radius: 8px 0px 0px 8px;
         border: 1px solid #ffb4a4;
-        background: #2b2220;
+        background: ${this.btnBackground};
         border-right: 0;
       }
       .decrement-btn-minus {
        
         padding: 16px;
         position: relative;
-        background: #2b2220;
+        background: ${this.btnBackground};
         border-right: 0;
       }
       .decrement-btn-minus-h {
         position: absolute;
-        background: #ffb4a4;
+        background: ${this.minusPlusBackground};
         width: 14px;
         height: 2px;
         top: 50%;
@@ -89,7 +135,7 @@ class Stepper extends HTMLElement {
       }
       .value-display {
         display: inline-block;
-        padding: 10.9px 18px;
+        padding: 10.8px 18px;
         font-size: 14px;
         color: #ffb4a4;
         border: 1px solid #ffb4a4;
@@ -100,19 +146,19 @@ class Stepper extends HTMLElement {
         border-radius: 0px 8px 8px 0px;
         border: 1px solid #ffb4a4;
         border-left: 0;
-        background:#2b2220;
+        background: ${this.btnBackground};
         height: 40px;
       }
       .increment-btn-plus {
         padding: 16px;
         position: relative;
-        background: #2b2220;
+        background: ${this.btnBackground};
         border-left: 0;
       }
       .increment-btn-plus-v,
       .increment-btn-plus-h {
         position: absolute;
-        background: #ffb4a4;
+        background: ${this.minusPlusBackground};
       }
       
       .increment-btn-plus-v {
@@ -134,14 +180,27 @@ class Stepper extends HTMLElement {
       .decrement-btn , .increment-btn {
         cursor: pointer;
       }
-    `
-
-    shadow.appendChild(style);
-    console.log(style.isConnected);
-    shadow.appendChild(container);
-
-    this._value = initialValue;
+    `;
+    this.shadowRoot.appendChild(style);
   }
+  // shadow.appendChild(style);
+  // console.log(style.isConnected);
+  // shadow.appendChild(container);
+
+  updateDisplay() {
+    const valueDisplay = this.shadowRoot.querySelector('.value-display');
+    if (valueDisplay) {
+      valueDisplay.textContent = this._value;
+    }
+  }
+  disconnectedCallback() {
+    const decrementBtn = this.shadowRoot.querySelector('.decrement-btn');
+    const incrementBtn = this.shadowRoot.querySelector('.increment-btn');
+    decrementBtn.removeEventListener('click', this.decrementButtonClickHandler);
+    incrementBtn.removeEventListener('click', this.incrementButtonClickHandler);
+
+  }
+
   get value() {
     return this._value;
   }
@@ -161,16 +220,7 @@ class Stepper extends HTMLElement {
     const precision = step.toString().split('.')[1]?.length || 0;
     this.value = (Math.round(this._value / step) * step - step).toFixed(precision);
   }
-  updateDisplay() {
-    const valueDisplay = this.shadowRoot.querySelector('.value-display');
-    valueDisplay.textContent = this._value;
-  }
-  disconnectedCallback() {
-    decrementBtn.removeEventListener('click', decrementButtonClickHandler);
-    incrementBtn.removeEventListener('click', incrementButtonClickHandler);
-  }
 }
-
 customElements.define('stepper-component', Stepper);
 
 
